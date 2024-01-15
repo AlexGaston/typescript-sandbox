95abe8f44e886793f6fee5e813d0f3bc9a55d003
Alex Gaston <alecgaston@gmail.com>
2024-01-15 10:58:53 +0100

    Laboratorio Módulo 2

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,13 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+/*
+
+Tipos de datos y Operadores - Laboratorio:
+
+1. Quito los 18 euros de bebida al tiquet de la cena
+
+2. El resultado lo tendremos que dividir por todos los comensales para saber que hemos de pagar cada uno
+
+*/
+
+console.log((120 - 18) / 7);
